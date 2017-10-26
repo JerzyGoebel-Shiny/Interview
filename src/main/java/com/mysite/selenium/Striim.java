e45b0ef24230ef790ad7d6d46fd9c2cc0c43a431
TrailBl4zer <31038145+TrailBl4zer@users.noreply.github.com>
2017-10-27 00:19:55 +0200

    Initial Commit

---
+++ src/main/java/com/mysite/selenium/Striim.java
@@ -0,0 +1,13 @@
+package com.mysite.selenium;
+
+import org.openqa.selenium.By;
+
+public class Striim {
+    public static final By CSS_BOX_USER = By.ByCssSelector.cssSelector("input[name=\"username\"]");
+    public static final By CSS_BOX_PASS = By.ByCssSelector.cssSelector("input[name=\"password\"]");
+    public static final By CSS_LOG_BUTN = By.ByCssSelector.cssSelector("button[type=\"submit\"]");
+    public static final By TAB_HOOK = By.id("view-title-text");
+    public static final String EXP_TAB      = "Home";
+
+    //public static final
+}
